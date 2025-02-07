8d0890464fb3daeac374bd4bef7763d1881bb24f
Diego Narrea Mori <diego.narrea@ext.blaze.me>
2025-02-07 12:48:37 -0500

    second 2 commit

--- invitacion.js
+++ invitacion.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     var animationExecuted = false; 
     var clickCount = 0;
-    var btnPressed = false;
-   
+    var isNoPressed = false;
+    var isYesPressed = false;
 
     $('.container').click(function () {
         clickCount++; 
@@ -33,18 +33,18 @@ $(document).ready(function () {
             });
         }
         else if (clickCount === 3) {
-            $('.card').html('<div class="text">Amor de mi vida y de mi corazon <br>Lo mejor tarda por llegar y sabes lo mucho que te amo</div>');
+            $('.card').html('<div class="text">Amor de mi vida y de mi corazon! <br>Lo mejor tarda por llegar y sabes lo mucho que te amo</div>');
             $('.text').css({
                 'margin-left': '-70px' ,
-                'font-size': '22px',
+                'font-size': '18px',
                 'padding': '5px 20px',
             });
         }
         else if (clickCount === 4) {
-            $('.card').html('<div class="text">Contigo todos los dias son muy especiales<br> Sabes lo muy feliz que me haces y siempre te lo digo.</div>');
+            $('.card').html('<div class="text">Contigo todos los dias son muy especiales ♡ <br> Sabes lo muy feliz que me haces y siempre te lo digo.</div>');
             $('.text').css({
                 'margin-left': '-70px' ,
-                'font-size': '22px',
+                'font-size': '18px',
                 'padding': '5px 20px',
             });
         }
@@ -52,7 +52,7 @@ $(document).ready(function () {
             $('.card').html('<div class="text">Y aún más especial será si me das tu respuesta a esto…</div>');
             $('.text').css({
                 'margin-left': '-70px' ,
-                'font-size': '22px',
+                'font-size': '18px',
                 'padding': '5px 20px',
             });
         }
@@ -60,38 +60,42 @@ $(document).ready(function () {
             $('.card').html('<div class="text">¿Te gustaría compartir este San Valentín conmigo?</div>');
             $('.text').css({
                 'margin-left': '-70px',
-                'font-size': '22px',
+                'font-size': '18px',
                 'padding': '5px 20px',
             });
-            $('.card').append('<div class="btn-container"><button class="btn-red">Sí</button><button class="btn-red">Si</button></div>');
-            $('.btn-red').eq(1).click(function() {
-                btnPressed = true;
-                $('.card').html('<div class="text">Estás destinada a estar a mi lado, sorry <br> Nos vemos el 11 y 14, amor de mi vida</div>');
-                
+            $('.card').append('<div class="btn-container"><button class="btn-red">Sí</button><button class="btn-red">No</button></div>');
+            $('.btn-red').eq(0).click(function() {
+                isYesPressed = true;
+                $('.card').html('<div class="text">Te amo mucho mi amor, nos vemos el 14 y el 15 en el templo. Y dale U!</div>');
                 $('.text').css({
                     'margin-left': '-70px',
-                    'font-size': '22px',
+                    'font-size': '18px',
                     'padding': '5px 20px',
                 });
             });
         
             $('.btn-red').eq(1).click(function() {
-                btnPressed = true;
-                $('.card').html('<div class="text">Estás destinada a estar a mi lado, sorry <br> Nos vemos el 11 y 14, amor de mi vida</div>');
-                
-                $('.text').css({
-                    'margin-left': '-70px',
-                    'font-size': '22px',
-                    'padding': '5px 20px',
+                isNoPressed = true;
+                $('.card').html('<img src="https://i.postimg.cc/65jcWBWg/AA21-F5-FA-C20-F-4779-8376-956-B4-A34-CA95.jpg" alt="Imagen" class="card-image">');
+                $('.card-image').css({
+                    'width': '70px',  
+                    'max-width': '80%', 
                 });
             });
         }
-        else if (clickCount >= 8 && btnPressed) {
-
+        else if (clickCount >= 8 && isYesPressed) {
             $('.card').html('<img src="https://i.postimg.cc/4d7GKHJH/IMG-4409.jpg" alt="Imagen" class="card-image">');
             $('.text').css({
                 'margin-left': '-70px',
-                'font-size': '22px',
+                'font-size': '18px',
+                'padding': '5px 15px',
+            });
+        }
+        else if (clickCount >= 8 && isNoPressed) {
+            $('.card').html('<div class="text">No hay un no como respuesta asi que igual es un si, nos vemos el 14 mi vida ♡ <br> Te amo!</div>');
+            $('.text').css({
+                'margin-left': '-70px',
+                'font-size': '18px',
                 'padding': '5px 15px',
             });
         }
